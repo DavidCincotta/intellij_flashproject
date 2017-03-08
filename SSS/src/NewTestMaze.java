ebb727486124cee5ae88393216164b5904cfdc6b
David Cincotta <decincotta@verizon.net>
2017-03-08 12:15:54 -0500

    updates

---
+++ SSS/src/NewTestMaze.java
@@ -0,0 +1,20 @@
+package lsrhs.java;
+
+public class NewTestMaze extends Maze {
+	private static final boolean newGrid[][] = {
+		{false, true,  false, false, false, true, false},
+		{false, true,  false, true,  false,  false,  false},
+		{false, true,  false, true,  false, true,  false},
+		{false, false,  false, false,  false, true,  false},
+		{false, true,  false, false, true, false,  false},
+		{false, true,  false,  true,  true,  false,  true},
+		{false, true, false, false, true, false, false}
+	};
+
+	public NewTestMaze() {
+		super();
+		grid = newGrid;
+	}
+
+
+}
---
+++ SimpleSort/src/MySorter.java
@@ -0,0 +1,10 @@
+package lsrhs.java;
+
+// sorter that needs to be completed by YOU, trying to figure out how to sort an array 
+public class MySorter extends SimpleSorterAbstract {
+	public int[] sort(int[] a) {
+		// IMPLEMENT ME!!!!!
+		return a;
+	}
+
+}
